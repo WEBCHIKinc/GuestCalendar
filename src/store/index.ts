7ae276cca89a310e6bbf2c8e6f1c8c170c90f791
WEBCHIKinc <kozerenkod@gmail.com>
2022-12-16 23:45:25 +0200

    npm, configere store

---
+++ src/store/index.ts
@@ -0,0 +1,15 @@
+import {
+  applyMiddleware,
+  combineReducers,
+  configureStore,
+} from "@reduxjs/toolkit";
+import thunk from "react-redux";
+
+const rootReducer = combineReducers({});
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
