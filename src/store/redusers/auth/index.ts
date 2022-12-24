26d37be7c948165044776a4e210422d6e70b3fdc
WEBCHIKinc <kozerenkod@gmail.com>
2022-12-24 19:45:42 +0200

    added new AuthAction details

--- src/store/redusers/auth/index.ts
+++ src/store/redusers/auth/index.ts
@@ -1,7 +1,11 @@
+import { IUser } from "../../../models/IUser";
 import { AuthAction, AuthActionEnum, AuthState } from "./types";
 
 const initialState: AuthState = {
   isAuth: false,
+  isLoading: false,
+  user: {} as IUser,
+  error: "",
 };
 
 export default function authReduser(
@@ -10,7 +14,13 @@ export default function authReduser(
 ): AuthState {
   switch (action.type) {
     case AuthActionEnum.SET_AUTH:
-      return { ...state, isAuth: action.payload };
+      return { ...state, isAuth: action.payload, isLoading: false };
+    case AuthActionEnum.SET_IS_LOADING:
+      return { ...state, isLoading: action.payload };
+    case AuthActionEnum.SET_ERROR:
+      return { ...state, error: action.payload, isLoading: false };
+    case AuthActionEnum.SET_USER:
+      return { ...state, user: action.payload };
     default:
       return state;
   }
--- src/store/redusers/auth/types.ts
+++ src/store/redusers/auth/types.ts
@@ -1,14 +1,38 @@
+import { IUser } from "../../../models/IUser";
+
 export enum AuthActionEnum {
   SET_AUTH = "SET_AUTH",
+  SET_IS_LOADING = "SET_IS_LOADING",
+  SET_ERROR = "SET_ERROR",
+  SET_USER = "SET_USER",
 }
 
 export interface AuthState {
   isAuth: boolean;
+  isLoading: boolean;
+  error: string;
+  user: IUser;
 }
 
 export interface SetAuthAction {
   type: AuthActionEnum.SET_AUTH;
   payload: boolean;
 }
+export interface SetIsLoadingAction {
+  type: AuthActionEnum.SET_IS_LOADING;
+  payload: boolean;
+}
+export interface SetErrorAction {
+  type: AuthActionEnum.SET_ERROR;
+  payload: string;
+}
+export interface SetUserAction {
+  type: AuthActionEnum.SET_USER;
+  payload: IUser;
+}
 
-export type AuthAction = SetAuthAction;
+export type AuthAction =
+  | SetAuthAction
+  | SetIsLoadingAction
+  | SetErrorAction
+  | SetUserAction;
